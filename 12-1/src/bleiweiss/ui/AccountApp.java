157ceaa1b90b30bf97259c178139ee102a2c3d17
Allyson Rosenthal <allyson_rosenthal@utexas.edu>
2016-02-19 22:10:40 -0600

    AccountApp

--- 12-1/src/bleiweiss/ui/AccountApp.java
+++ 12-1/src/bleiweiss/ui/AccountApp.java
@@ -1,7 +1,45 @@
 package bleiweiss.ui;
 
-/**
- * Created by Allyson on 2/19/16.
- */
+import bleiweiss.business.Account;
+import bleiweiss.business.CheckingAccount;
+import bleiweiss.business.Transactions;
+
 public class AccountApp {
+    public static void main(String args[]) {
+        String choice = "y";
+        Account account = new Account();
+        CheckingAccount checkingAccount = new CheckingAccount();
+
+        Console.displayLine("Welcome to the Account Calculator");
+        Console.displayLine();
+        Console.displayLine("Starting Balance");
+        Console.displayLine("Checking: " + account.getBalanceFormatted());
+        Console.displayLine();
+        Console.displayLine("Enter the transactions for the month");
+        Console.displayLine();
+
+        while (choice.equals("y")) {
+            Transactions transaction = new Transactions();
+            String action = Console.getString("Withdrawal or deposit? (w/d): ");
+            double amount = Console.getDouble("Amount: ");
+
+            if (action.equals("w")) {
+                transaction.withdraw(account, amount);
+
+            } else if (action.equals("d")) {
+                transaction.deposit(account, amount);
+
+            }
+
+            choice = Console.getString("Continue? (y/n): ");
+        }
+
+        Console.displayLine("Monthly Fees");
+        Console.displayLine("Checking fee:              " + checkingAccount.getMonthlyFeeFormatted());
+        Console.displayLine();
+        Console.displayLine("Final Balance");
+        Console.displayLine("Checking: " + account.getBalanceFormatted());
+
+
+    }
 }

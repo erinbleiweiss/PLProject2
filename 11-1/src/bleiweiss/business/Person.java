82080e6c40700624b205a8db9e35703f249f74e4
Allyson Rosenthal <allyson_rosenthal@utexas.edu>
2016-02-19 20:03:51 -0600

    Person class

--- 11-1/src/bleiweiss/business/Person.java
+++ 11-1/src/bleiweiss/business/Person.java
@@ -1,4 +1,45 @@
 package bleiweiss.business;
 
-public class Person {
+public abstract class Person {
+
+    String firstName;
+    String lastName;
+    String email;
+
+    public Person() {
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + getFirstName() + " " + getLastName() +
+                "\nEmail: " + getEmail();
+    }
+
+    public abstract String getDisplayText();
+    
 }

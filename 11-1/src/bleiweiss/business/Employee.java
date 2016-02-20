955794c0290a8e83e13bafb82e2403e73c15e03b
Erin Bleiweiss <erin.bleiweiss@utexas.edu>
2016-02-19 20:12:23 -0600

    employee class

--- 11-1/src/bleiweiss/business/Employee.java
+++ 11-1/src/bleiweiss/business/Employee.java
@@ -1,4 +1,24 @@
 package bleiweiss.business;
 
 public class Employee {
+
+    private String ssn;
+
+    public Employee(){
+        super();
+        ssn = "";
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getDisplayText(){
+        return super.toString() + "\nSocial security number: " + getSsn();
+    }
+
 }
